debaa6b9fd9973af50017bad95a252c7511e1b2c
David Ambrozio <davidambroziodev@gmail.com>
2023-10-09 20:05:05 -0300

    fix base.py

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 class CustomBaseModel(BaseModel):
-    def dict(*args, **kwargs):
-        d = super().dict(*args, **kwargs)
+    def dict(self, *args, **kwargs):
+        d = super().model_dump(*args, **kwargs)
         d = {k: v for k, v in d.items() if v is not None}
         return d
